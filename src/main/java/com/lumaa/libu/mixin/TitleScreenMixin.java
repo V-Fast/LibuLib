47ea020e3dd6b4d5a2690ae0a669dd973c7d6819
Lumaa <nathan@ama49.fr>
2022-12-29 14:12:53 +0100

    lowered buttons

--- src/main/java/com/lumaa/libu/mixin/TitleScreenMixin.java
+++ src/main/java/com/lumaa/libu/mixin/TitleScreenMixin.java
@@ -11,6 +11,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.io.IOException;
+import java.util.Locale;
 
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin extends Screen {
@@ -25,10 +26,10 @@ public class TitleScreenMixin extends Screen {
         // update all the mods using LibuLib (including LibuLib)
         if (LibuLib.getUpdates().size() > 0) {
             LibuLib.getUpdates().forEach(update -> {
-                LibuLib.logger.info("[LibuLib] UpdateChecker - Checking " + update.getMod().name);
+                if (!update.isShown()) LibuLib.logger.info("[LibuLib] UpdateChecker - Checking " + update.getMod().name);
                 try {
                     if (!update.getString("version_number").equals(update.getMod().versionId) && !update.isShown()) {
-                        if (update.getMod().versionId == "Dev") {
+                        if (update.getMod().versionId.toLowerCase(Locale.ROOT).trim() == "dev") {
                             update.setShown(true);
                             LibuLib.logger.info(update.getMod().name + "'s Developer version prevented an update.");
                         } else {
