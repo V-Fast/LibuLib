bd8408a0ca6f094fbb6e54fb80b24c9e6fa0249b
Lumaa <nathan@ama49.fr>
2023-04-19 12:09:31 +0200

    more settings

--- src/main/java/com/lumaa/libu/client/ui/LibuToast.java
+++ src/main/java/com/lumaa/libu/client/ui/LibuToast.java
@@ -2,6 +2,9 @@ package com.lumaa.libu.client.ui;
 
 import com.lumaa.libu.util.Color;
 import com.mojang.blaze3d.systems.RenderSystem;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.toast.Toast;
 import net.minecraft.client.toast.ToastManager;
@@ -9,14 +12,16 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.MutableText;
 
+@Environment(EnvType.CLIENT)
 public class LibuToast implements Toast {
     public ItemStack icon;
     public MutableText title;
     public MutableText description;
+    public ToastType type = ToastType.NORMAL;
 
     private static int titleColor = Color.white;
     private static int descColor = 0xffff55;
-    private boolean displayed = false;
+    private boolean displayed;
 
     public LibuToast(ItemStack icon, MutableText title, MutableText description) {
         this.icon = icon;
@@ -25,9 +30,14 @@ public class LibuToast implements Toast {
         this.displayed = false;
     }
 
-    public void display(ToastManager toastManager) {
+    public void textColor(int titleColor, int descColor) {
+        LibuToast.titleColor = titleColor;
+        LibuToast.descColor = descColor;
+    }
+
+    public void display() {
         if (!displayed) {
-            toastManager.add(this);
+            MinecraftClient.getInstance().getToastManager().add(this);
             this.displayed = true;
         }
     }
@@ -38,12 +48,24 @@ public class LibuToast implements Toast {
 
         RenderSystem.setShaderTexture(0, TEXTURE);
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-        manager.drawTexture(matrices,  0, 0, 0, 0, getWidth(), getHeight());
+        manager.drawTexture(matrices,  0, 0, 0, type.v, getWidth(), getHeight());
         textRenderer.draw(matrices, this.title, 30.0F, 7.0F, titleColor);
         textRenderer.draw(matrices, this.description, 30.0F, 18.0F, descColor);
         manager.getClient().getItemRenderer().renderInGui(matrices, icon, 8, 8);
 
         this.displayed = startTime < 5000L;
         return startTime >= 5000L ? Visibility.HIDE : Visibility.SHOW;
+    }
+
+    public enum ToastType {
+        NORMAL(0),
+        WHITE(32),
+        WARNING(64);
+
+        public int v;
+
+        ToastType(int v) {
+            this.v = v;
+        }
     }
 }
