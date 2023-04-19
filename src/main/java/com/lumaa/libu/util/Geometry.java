8e0db9871148cf270e0c3cfec3f993a7b560947c
Lumaa <nathan@ama49.fr>
2023-04-19 12:07:48 +0200

    separated geometries

--- src/main/java/com/lumaa/libu/util/Geometry.java
+++ src/main/java/com/lumaa/libu/util/Geometry.java
@@ -1,6 +1,66 @@
 package com.lumaa.libu.util;
 
+import com.lumaa.libu.LibuLibClient;
+import net.minecraft.util.math.BlockPos;
+
 public class Geometry {
+    public static class Coordinate {
+        private int x;
+        private int y;
+        private int z;
+
+        private static boolean is3d = true;
+
+        public Coordinate(int x, int y) {
+            this.x = x;
+            this.y = y;
+
+            setIs3d(false);
+        }
+
+        public Coordinate(int x, int y, int z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+
+            setIs3d(true);
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public void setX(int x) {
+            this.x = x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public void setY(int y) {
+            this.y = y;
+        }
+
+        public int getZ() throws Exception {
+            if (!is3d()) throw new Exception("Cannot get Z coordinates in a 2D space");
+            return z;
+        }
+
+        public void setZ(int z) {
+            this.z = z;
+            setIs3d(true);
+        }
+
+        public static void setIs3d(boolean is3d) {
+            Coordinate.is3d = is3d;
+        }
+
+        public static boolean is3d() {
+            return is3d;
+        }
+    }
+
     public static class Translation2d {
         private int x1 = 0;
         private int x2;
@@ -21,22 +81,28 @@ public class Geometry {
             return this;
         }
 
-        public Translation2n getOriginCoordinates() {
-            return new Translation2n(x1, y1);
+        public Coordinate getOriginCoordinates() {
+            return new Coordinate(this.x1, this.y1);
         }
 
-        public Translation2n getEndCoordinates() {
-            return new Translation2n(x2, y2);
+        public Coordinate getEndCoordinates() {
+            return new Coordinate(this.x2, this.y2);
         }
-    }
 
-    public static class Translation2n {
-        public int x;
-        public int y;
+        public int getOriginX() {
+            return x1;
+        }
 
-        private Translation2n(int x, int y) {
-            this.x = x;
-            this.y = y;
+        public int getEndX() {
+            return x2;
+        }
+
+        public int getOriginY() {
+            return y1;
+        }
+
+        public int getEndY() {
+            return y2;
         }
     }
 
@@ -65,4 +131,24 @@ public class Geometry {
             return this;
         }
     }
+
+    public static class Scale3d {
+        public Coordinate posA;
+        public Coordinate posB;
+
+        public Scale3d(Coordinate positionA, Coordinate positionB) {
+            this.posA = positionA;
+            this.posB = positionB;
+        }
+
+        public MinecraftGeometry.Scale3d toBlockPos() {
+            try {
+                return new MinecraftGeometry.Scale3d(new BlockPos(posA.getX(), posA.getY(), posA.getZ()), new BlockPos(posB.getX(), posB.getY(), posB.getZ()));
+            } catch (Exception e) {
+                LibuLibClient.logger.error(e.getMessage());
+                return null;
+            }
+        }
+    }
 }
+
